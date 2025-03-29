103ec6340222872c5f73f4fafcfc2a2be3f506e2
Ishan Sheth <contact@imsheth.com>
2025-03-28 20:33:46 -0400

    Adds: fixes build

--- bhumikasheth/astro.config.mjs
+++ bhumikasheth/astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bhumikasheth.github.io",
-  base: "/",
+  site: "https://bhumikasheth.com",
+  base: "/bhumikasheth.github.io",
   output: "static",
+  build: {
+    assets: "astro",
+  },
 });
